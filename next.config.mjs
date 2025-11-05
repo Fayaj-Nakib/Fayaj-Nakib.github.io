454c26df1311e69ffa674753eb3410788c93478c
Md Fayaj Nakib <nakibfayaj99@gmail.com>
2025-11-05 19:30:48 +0600

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repo name is different from fayaj-nakib.github.io, add basePath
+  // basePath: '/your-repo-name',
+  // trailingSlash: true,
 };
 
 export default nextConfig;
